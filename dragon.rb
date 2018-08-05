f5c898751eb18bb67037535e87221a412ee13254
Josh Thompson <thompsonjoshd@gmail.com>
2018-08-05 15:35:47 -0500

    feedback on dragon

--- dragon.rb
+++ dragon.rb
@@ -1,14 +1,26 @@
 class Dragon
   attr_accessor :name, :color, :rider, :hungry, :hunger
+  # try changing this back to attr_reader
   def initialize(name, color, rider)
     @name = name
     @color = color
     @rider = rider
     @hungry = true
+    # i'd also avoid making this an attribute, but actually a method
+    # called def hungry. I'll explain more there
     @hunger = 3
+    # try changing this to "meals_count" or something
+    # then, check out my notes in `def eat`
+  end
+  
+  def hungry
+    return true if @meals_count < 3
+    false
   end
 
   def eat
+    # have this += the @meals_count variable by one
+    
     if @hunger > 0
        @hunger -= 1
        @hungry = false if @hunger == 0
